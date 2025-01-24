0f1b867d06bd7f898e0bb93a39085f3b5dd54ae9
HarryJohn <harryjohn5093421@gmail.com>
2025-01-24 17:09:36 -0500

    88888added

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import { LandingPage } from "./landingcomponents/LandingPage";
 const App = () => {
   // const [alertState, setAlertState] = useState(false);
   // const alert = useContext(MyContext);
-  //888
+  //88888
   const setAlert = (obj) => {
     // setAlertState(obj.status);
     if (obj.status === "success")
